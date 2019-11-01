1936557dffa57f6e0ae1d812030a7012df12b8e2
Ramtin Aryan <ramary@oslomet.no>
2019-11-01 22:36:50 +0100

    Create toplogy Json config file based on the inputs. 	It creates a topology with the spicific length and number of neighbors for each node. ## To make a commit, type your commit message and press CTRL-ENTER. ## To cancel the commit, close the window. To sign off on the commit, ## press CTRL-S. ## ## You may also reference or close a GitHub issue with this commit. ## To do so, type `#` followed by the `tab` key.  You will be shown a ## list of issues related to the current repo.  You may also type ## `owner/repo#` plus the `tab` key to reference an issue in a ## different GitHub repo.
     topo_config_generator.py | 44 ++++++++++++++++++++++++++++++++++++++++++++
     1 file changed, 44 insertions(+)

---
+++ topo_config_generator.py
@@ -0,0 +1,44 @@
+import json
+import sys
+
+outPutFileName = ""
+
+
+def createTopo(path_length, neighbor_count, ctrIP, ctrPort, outputFile):
+    """Create a Json file which is an input for the topo_generator.py """
+    outPutFileName = outputFile
+    ruleList = list()
+    topo = {}
+    topo['switch_No'] = path_length + (path_length * neighbor_count)
+    topo['switchs'] = []
+    topo['links'] = []
+    topo['controller_IP'] = str(ctrIP)
+    topo['controller_Port'] = ctrPort
+
+    for node in range(1, path_length + 1):
+        topo['switchs'].append(node)
+        if node != path_length:
+            link = str(node) + ":" + str(node + 1)
+            topo['links'].append(link)
+
+        for neighbor in range(1, neighbor_count + 1):
+            link = str(node) + ":" + str(node) + str(neighbor) + "00"
+            topo['links'].append(link)
+            topo['switchs'].append(int(str(node) + str(neighbor) + "00"))
+
+    with open(outPutFileName, 'w', newline='\n') as topoFile:
+        json.dump(topo, topoFile, indent=3, separators=(',', ':'))
+        topoFile.close()
+
+
+if __name__ == '__main__':
+        if len(sys.argv) == 6:
+     createTopo(int(sys.argv[1]), int(sys.argv[2]),
+                str(sys.argv[3]), int(sys.argv[4]), str(sys.argv[5]))
+     else:
+     print("Wrong input !\n \
+             Correct input format: path-length  neighbor  cnotroller_IP  controller_Port outputFile .\n \
+             path-length:number of switches on the main path    neighbor: number of neighbor for each switch  controller_IP: controller IP address\
+               controller_Port: controller port number  outputFile: address and name of the outputFile")
+
+#    createTopo(10, 1, "158.36.99.24", 6623, "topology.json")
