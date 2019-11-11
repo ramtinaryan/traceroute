a9511c9cf36de434d0d93e63ad5bd6dc4ffa47be
Ramtin Aryan <ramary@oslomet.no>
2019-11-11 11:15:14 +0100

    Refactoring remove file.close()

--- probing.py
+++ probing.py
@@ -3,12 +3,11 @@ import json
 
 
 def getQueries():
-	fileName="/home/ramary/tracerout/query.json"
-	listOfQueries=list()
-	with open(fileName, 'r') as f:
-	    loaded_queries = json.load(f)
-	f.close()
-	for query in loaded_queries:
-		listOfQueries.append(query)
-	return listOfQueries
+    fileName = "/home/ramary/tracerout/query.json"
+    listOfQueries = list()
+    with open(fileName, 'r') as f:
+        loaded_queries = json.load(f)
 
+    for query in loaded_queries:
+        listOfQueries.append(query)
+    return listOfQueries
